e7c32b50fbacf74fdee14a858c23c41f00b7e982
Thiago Brito <deoliveira.tbrito@gmail.com>
2024-01-10 21:56:20 -0300

    vel koz blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,44 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "O OLHO DO VAZIO VEL'KOZ",
+    "description": "Para realmente entender o horror que é Vel’Koz, é necessário que se saiba sobre os Observadores e como eles ficaram cegos para o mundo mortal.",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Velkoz_0.jpg"
+  },
+  {
+    "id":"2",
+    "title": "VEL'KOZ MÁQUINA DE COMBATE",
+    "description": "",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Velkoz_1.jpg"
+  },
+  {
+    "id":"3",
+    "title": "VEL'KOZ CELESTE",
+    "description": "",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Velkoz_2.jpg"
+  },
+  {
+    "id":"4",
+    "title": "CERTAMENTE NÃO É O VEL'KOZ",
+    "description": "",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Velkoz_3.jpg"
+  },
+  {
+    "id":"5",
+    "title": "VEL'KOZ INFERNAL",
+    "description": "",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Velkoz_4.jpg"
+  },
+  {
+    "id":"6",
+    "title": "VEL'KOZ GELO SOMBRIO",
+    "description": "",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Velkoz_11.jpg"
+  },
+  {
+    "id":"7",
+    "title": "MEL'KOZ",
+    "description": "",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Velkoz_20.jpg"
+  }
+]
